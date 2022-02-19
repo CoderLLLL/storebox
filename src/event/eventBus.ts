e4292e18b46d235e3dcd15e44cec52a82ae98dd9
CoderLLLL <18194024796@163.com>
2022-02-19 22:14:00 +0800

    零星更新-给代码做了注释-无描述

--- src/event/eventBus.ts
+++ src/event/eventBus.ts
@@ -7,6 +7,18 @@ export default class HREventBus {
     constructor(){
         this.eventBus = {}
     }
+    /* 
+        on方法：这里会接收三个值，一个是事件名，事件名就是和我们要监听的数据名或者key是一样的，因为我们可能监听多个值，
+                所以支持传入多个值-数组，然后是外部依赖的函数，这个会接收一个函数，然后当我们要监听这个值的时候，我们
+                就会将这个函数放入到对应的依赖数组里面去，最后接收的值就是this指向
+
+                当我们会将这些依赖函数放到一个数组里面去，还有对应其它依赖于这个值的函数也会放入到这个里面去，然后会创建
+                一个新的数组去保存这些依赖这个值的key，到时候监听到一个数组的某个值被改变了，那么除了监听本事的值之外，我
+                们还要拿到数组的其它值，这时候就直接遍历刚刚保存的key的数组即可拿到对应的值，最后会创建一个对象，这里称之为桶
+                将依赖函数的数组和依赖的key装入这个桶中，最后key为事件名，value就是这个桶，存入到eventBus中
+
+                其中还包括是否是深层监听，如果是深层监听的值，那么会将这个值以及这个值的所有子元素都返回给用户中
+    */
 
     public on(eventName:string | string[],eventCallback:IEventFn,thisArgs:any = window){
         const execFn = (eventName:string,keys:string[] = []) =>{
@@ -38,6 +50,13 @@ export default class HREventBus {
         }        
     }
 
+    /* 
+        emit方法：这里会接收三个值：事件名、整个state（代理的state）、是否深层监听，然后执行了emit方法之后，如果事件名是一个字符串
+                 那么我们直接在state中取到对应的key即可，如果是一个数组，那么就要将这个数组的每一个key对应的值取到，
+                 如果是只有一个值，但是这个值是一个对象，所以这里应该是要深层监听的，所以我们要将这个state都返回给它，因为这里的深层
+                 监听是每一层都做了代理，所以这里接收到的target是这个事件名对应的对象和所有的子元素
+    */
+
     public emit<T>(eventName:string,target:T,isdeep:boolean = false){            
         let resArr:any[] = []            
         const bucket = this.eventBus[eventName]
@@ -58,6 +77,13 @@ export default class HREventBus {
             item?.eventCallback.apply(item.thisArgs,resArr)
         }
     }
+
+    /* 
+        off方法：这里我们要接收到两个值，一个是事件名，一个是我们要取消监听的值
+                因为我们在开发时，销毁（离开这个组件）的时候，这些监听的值可能被别的组件触发，但是这个组件已经用不上
+                之前监听的值了，所以要将对应的依赖函数及时清理，先取到对应的事件桶，然后取出函数依赖数组，然后找到对应
+                的函数，最后删除掉，然后如果这个桶是空的，那么就将这个桶删除掉
+    */
 
     public off(eventName:string,eventCallback:IEventFn){
         const bucket = this.eventBus[eventName]
--- src/event/eventStore.ts
+++ src/event/eventStore.ts
@@ -7,6 +7,14 @@ class HREventStore<T extends IkeyType> {
     private event
     public modules
 
+    /* 
+        构造器：创建actions、event、state，然后判断用户有没有写modules，有的话直接在state中创建modules对象，里面再实例一个store
+        这样是为了取出modules里面的actins时可以直接dispatch派发事件到对应的store，onState也是可以监听到对应的state的值
+
+        其中会给state做一次代理，返回代理对象，代理是深层监听的，所以state的值是对象或者数组，都能够监听到内部数据的变化
+        然后执行对应的emit的函数
+    */
+
     constructor(optinos:IStoreOptions<T>){
         this.actions = optinos.actions
         this.event = new HREventBus()
@@ -27,6 +35,13 @@ class HREventStore<T extends IkeyType> {
         
     }
 
+    /* 
+        _observe方法：给state做代理，最后返回一个代理对象，实现的原理是使用递归，使用深度优先算法实现每一层的监听
+
+        设置get属性，就直接将对应的值返回给用户即可
+        设置set属性，如果要设置的值和之前的一样，那么就没有必要再去修改和触发对应的emit函数，
+        如果不一样，那么就需要修改，然后将state值传入到emit，然后emit方法会取出对应的value
+    */
     private _observe(state:T['state']){
         const _this = this
         const obj = JSON.parse(JSON.stringify(state))
@@ -42,11 +57,21 @@ class HREventStore<T extends IkeyType> {
             }
         })
 
+        //调用深层代理方法，做深层代理
         proxyObj = this._deepAgent(proxyObj)
 
         return proxyObj
     }
 
+
+    /* 
+        _deepAgent方法：深层代理方法，调用这个方之后，为了防止传入为空，所以为空会抛出一个异常，然后调用内部递归深度监听方法_nextAgent
+
+        state里面的对象值被修改的时候，最终事件都会发送到state的这个对象中，因为这里是不知道用监听这个对象究竟是哪个函数
+        对这个对象做了修改，所以只能直接执行这个对象的所有函数依赖
+
+        所有要传入这个对象的key，后面子元素被修改后就会通过emit来执行这个对象的所有依赖函数
+    */
     private _deepAgent(proxyObj:IResultObj){  
         const proxyKey = Object.keys(proxyObj)
         if(proxyKey.length === 0) throw new Error('传入需要代理的对象为空')
@@ -56,6 +81,14 @@ class HREventStore<T extends IkeyType> {
         return proxyObj
     }
 
+    /* 
+        _nextAgent方法：内部深度监听方法，遍历传入的对象中的每一个元素，是对象的话，那么就会将这个对象做一次代理，并且将这个对象换成代理对象
+        这样做的目的是---当我们的对象里面的某个值修改之后，我们能够监听到这个对象的值被改变，然后就可以触发事件了
+
+        最终触发的事件就是就是传入对象的key
+
+        这里采用了深度优先算法，先对对每一层的第一个元素做代理，代理完成之后直接递归到下一层去代理
+    */
     private _nextAgent(proxyObj:IResultObj,rootKey:string){
         const proxyKey = Object.keys(proxyObj)
         if(proxyKey.length === 0) return
@@ -68,6 +101,10 @@ class HREventStore<T extends IkeyType> {
         }
     }
 
+    /* 
+        _proxy方法：内部代理方法，在内部递归方法中会调用这个方法，这里的实现思路和前面的代理方法是一样的，只不过这里
+        是做深度监听的
+    */
     private _proxy(proxyObj:IResultObj,rootKey:string){
         const _this = this
         return new Proxy(proxyObj,{
@@ -82,6 +119,23 @@ class HREventStore<T extends IkeyType> {
             }
         })
     }
+
+    /* 
+        onState方法，监听方法，这里会接收两个个值，
+        第一个值是要监听的值，其中这个值是在state里面的，也可以接收一个数组，因为我们想要监听的值可能不止一个
+        第二个值就是回调函数，用户通过这个函数能够拿到对应的监听值，然后就可以通过函数来使用这些值
+
+        当调用这个监听方法的时候，会立即执行一次，并且将对应的的值从state取出，然后返回给传入的函数，并且做on监听
+        当值被修改的时候，这个函数会被重新执行，所以用户拿到的值也是新的，而且用户那边使用这些是会被重新赋值，这是这个库
+        响应式的核心原理
+
+        但是这里要做情况判断，当传入的是一个普通值的字符串类型，也就是一个state的一个key的时候，这里立即执行函数并且直接返回对应的值
+            如果是一个数组，那么就遍历数组的每一个key，将每一个key的value取出按照顺序保存到数组，最后再去执行这个函数，并且将前面的数组传入
+        到函数里面去，这样函数就能拿到前面传入数组中每个key的value了
+            最后一个情况，就是监听到modules，这里会将这个取出对应的store，然后将store回传给我们的传入的函数，然后再去再去onState就可以拿到里面的
+        值和actions
+        ------------------------------------注意：这里会形成回调地狱，在后续版本中要优先解决这个问题
+    */
 
     public onState(stateKey:keyof T['state'] | 'modules' | string[],stateCallBack:IEventFn){                    
         if(typeof stateKey === 'string' && stateKey !== ''){            
@@ -112,9 +166,17 @@ class HREventStore<T extends IkeyType> {
         }
     }
 
+    /* 
+        setState方法：修改state方法，修改对应store里面state的key
+    */
     public setState(stateKey:keyof T['state'],newValue:any){
         this.state[stateKey] = newValue
     }
+
+    /* 
+        offState方法：取消监听方法，因为是要取消state对应key监听，所以要先判断key是否在state中
+        然后再去判断是否是数组，如果是数组，那么久一个个的去取消监听
+    */
 
     public offState(stateKey:keyof T['state'] | string[],stateCallBack:IEventFn){
         if(typeof stateKey === 'string' && stateKey !== ''){
@@ -131,6 +193,11 @@ class HREventStore<T extends IkeyType> {
         }
     }
 
+    /* 
+        dispatch方法：事件派发方法，我们也要先确定用户传入的事件是否在actions对象里面
+        然后拿到对应的actions，再去执行，并且再将用户传入的额外参数传入到事件函数中即可
+        ---------------------------------注意：这里是模块的时候，还没有将根的state传入到函数中，后期要实现
+    */
     public dispatch(actionName:string,...arg:any[]){
         if(!this.actions) return
         if(Object.keys(this.actions).indexOf(actionName) === -1) throw new Error('输入的方法不在actions中')
